7ffe578b97dee4061341b4939e340ff04dd10b01
Vinicius Leao <vinicius.s.silva-kee@parceirosedu.com.br>
2023-08-30 17:51:54 -0300

    feat: add modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,18 @@
+import * as Dialog from '@radix-ui/react-dialog';
 import logoImg from '../../assets/logo.svg';
+import { NewTransactionModal } from '../NewTransactionModal';
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logoImg} alt="" />
-        <NewTransactionButton>Nova Transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova Transação</NewTransactionButton>
+          </Dialog.Trigger>
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
